d09bbd0e9de2341f992d7b7d6b07ec2025f7a70f
mielindev <phamvietquang.dev@gmail.com>
2024-08-18 11:15:14 +0700

    update 5 part exercise

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 //Code here
+var originArray = [];
+function submit() {
+  var valueArr = document.querySelector("#number").value * 1;
+  originArray.push(valueArr);
+  document.querySelector(".result").innerHTML = `Mảng có: ${originArray}`;
+  document.querySelector("#number").value = "";
+}
+function sumYangNum() {
+  var arrLength = originArray.length;
+  var sum = 0;
+  for (var index = 0; index < arrLength; index++) {
+    if (originArray[index] > 0) {
+      sum += originArray[index];
+    }
+  }
+  document.querySelector(
+    "#sumYangNum"
+  ).innerHTML = `Tổng các số dương bằng: ${sum}`;
+}
+function countYangNum() {
+  var arrLength = originArray.length;
+  var count = 0;
+  for (var index = 0; index < arrLength; index++) {
+    if (originArray[index] > 0) {
+      count++;
+    }
+  }
+  document.querySelector(
+    "#countYangNum"
+  ).innerHTML = `Tổng các số dương bằng: ${count}`;
+}
+function minArr() {
+  var arrLength = originArray.length;
+  var minValue = originArray[0];
+  for (var index = 1; index < arrLength; index++) {
+    if (originArray[index] < minValue) {
+      minValue = originArray[index];
+    }
+  }
+  document.querySelector(
+    "#minArr"
+  ).innerHTML = `Số nhỏ nhất trong mảng: ${minValue}`;
+}
+function minYangArr() {
+  var arrLength = originArray.length;
+  var newArr = [];
+  for (var index = 0; index < arrLength; index++) {
+    if (originArray[index] > 0) {
+      newArr.push(originArray[index]);
+    }
+  }
+  var minYangValue = newArr[0];
+  for (var index = 1; index < newArr.length; index++) {
+    if (newArr[index] < minYangValue) {
+      minYangValue = newArr[index];
+    }
+  }
+  document.querySelector(
+    "#minYangArr"
+  ).innerHTML = `Số nhỏ nhất trong mảng: ${minYangValue}`;
+}
+function lastEvenNum() {
+  var arrLength = originArray.length;
+  var lastEvenNum = -1;
+  for (var index = 0; index < arrLength; index++) {
+    if (originArray[index] % 2 == 0) {
+      lastEvenNum = originArray[index];
+    }
+  }
+  document.querySelector(
+    "#lastEvenNum"
+  ).innerHTML = `Số nhỏ nhất trong mảng: ${lastEvenNum}`;
+}
