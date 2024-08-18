7a74ced3e31b9aaf7f0a60df60b917c1c86d109d
mielindev <phamvietquang.dev@gmail.com>
2024-08-18 17:16:17 +0700

    complete JS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Code here
 var originArray = [];
+var realNumArr = [];
 function submit() {
   var valueArr = document.querySelector("#number").value * 1;
   originArray.push(valueArr);
@@ -81,4 +82,80 @@ function swapLocation() {
   document.querySelector(
     "#swapLocation"
   ).innerHTML = `Mảng sau khi đổi vị trí: ${originArray}`;
+}
+
+function sortUp() {
+  var newArr = originArray;
+  var newArrLength = newArr.length;
+  for (var i = 0; i < newArrLength; i++) {
+    var temporary = null;
+    for (var j = i + 1; j < newArrLength; j++) {
+      if (newArr[i] > newArr[j]) {
+        temporary = newArr[i];
+        newArr[i] = newArr[j];
+        newArr[j] = temporary;
+      }
+    }
+  }
+  document.querySelector("#sortUp").innerHTML = `Mảng đã sắp xếp: ${newArr}`;
+}
+function isPrime(n) {
+  let prime = true;
+  if (n < 2) return (prime = false);
+  let i = 2;
+  while (i < n) {
+    if (n % i == 0) {
+      prime = false;
+      break;
+    }
+    i++;
+  }
+  return prime;
+}
+
+function findPrime() {
+  for (var index = 0; index < originArray.length; index++) {
+    console.log(isPrime(originArray[index]));
+    if (isPrime(originArray[index]) == true) {
+      document.querySelector("#findPrime").innerHTML = `${originArray[index]}`;
+      break;
+    } else {
+      document.querySelector("#findPrime").innerHTML = `Không có số nguyên tố`;
+    }
+  }
+}
+
+function addRealNum() {
+  var valueArr = document.querySelector("#realNumber").value * 1;
+  realNumArr.push(valueArr);
+  document.querySelector("#realNum").innerHTML = `Mảng có: ${realNumArr}`;
+  document.querySelector("#realNumber").value = "";
+}
+
+function countRealNum() {
+  var count = 0;
+  for (var index = 0; index < realNumArr.length; index++) {
+    if (Number.isInteger(realNumArr[index])) {
+      count++;
+    }
+  }
+  document.querySelector("#countRealNum").innerHTML = `${count}`;
+}
+function comparePosNega() {
+  var countPos = 0;
+  var countNega = 0;
+  for (var index = 0; index < originArray.length; index++) {
+    if (originArray[index] > 0) {
+      countPos++;
+    } else {
+      countNega++;
+    }
+  }
+  if (countPos == countNega) {
+    document.querySelector("#comparePosNega").innerHTML = `Số dương = Số âm`;
+  } else if (countPos > countNega) {
+    document.querySelector("#comparePosNega").innerHTML = `Số dương > Số âm`;
+  } else {
+    document.querySelector("#comparePosNega").innerHTML = `Số dương < Số âm`;
+  }
 }
